10484442e89693bcfbcd075be2e8c2bfdd435453
Hetvik Patel <146126236+Hetvik0907@users.noreply.github.com>
2025-06-29 02:16:14 +0530

    Update axiosinstance.js

--- Frontend/src/utils/axiosinstance.js
+++ Frontend/src/utils/axiosinstance.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+const API = import.meta.env.VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
-    baseURL:"import.meta.env.VITE_API_BASE_URL",
+    baseURL:API,
     timeout:10000, //10s
     withCredentials:true
 })
